a7fced32da928e0219b7e75d409be85b4bd70745
Crazelu <eberelucky64@gmail.com>
2023-11-28 07:12:20 +0100

    Initial Commit

---
+++ MyFlix/MyFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFlixApp.swift
+//  MyFlix
+//
+//  Created by LUCKY EBERE on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
